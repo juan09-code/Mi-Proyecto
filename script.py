0c9f411f5a81ec33ad8810fe29494e5203e75893
Juan Salgado <jsalgadoa@est.ups.edu.ec>
2020-12-15 10:43:52 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,14 @@
+#Script de prueba para Git
+#Autor: Juan Salgado
+#Fecha: 15/12/2020
+#Version: 1.0.0
+
+
+def print_hi(name):
+    print(f'Hola Mundo, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
